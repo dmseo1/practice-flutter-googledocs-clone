46704103622f4a3292f63cd4c87e8238270d15ac
Ryan Seo <seodongmany@gmail.com>
2023-02-08 20:44:43 +0900

    구글 로그인 취약점 수정 & 로그인 처리 및 화면 전환

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,31 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
+import 'package:practice_flutter_googledocs_clone/constants.dart';
+import 'package:practice_flutter_googledocs_clone/models/error_model.dart';
+import 'package:practice_flutter_googledocs_clone/models/user_model.dart';
 
 final authRepositoryProvider = Provider(
-  (ref) => AuthRepository(
-    googleSignIn: GoogleSignIn(),
-  ),
+  (ref) => AuthRepository(googleSignIn: GoogleSignIn(), client: Client()),
 );
 //ref: 다른 provider와 interaction할 때 매우 유용. interaction 할 일이 없으면 _ 로 두어도 됨.
-//Provider 왜 쓰는가? : GoogleSignIn 인스턴스를 쉽게 생성하고, 테스트가 용이해지기 때문
+//Provider 왜 쓰는가? : 인스턴스를 쉽게 생성하고, 테스트가 용이해지기 때문
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
 
   AuthRepository({
     required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+    required Client client,
+  })  : _googleSignIn = googleSignIn,
+        _client = client;
 
-  void signInWithGoogle() async {
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error =
+        ErrorModel(error: 'Some unexpected error occured', data: null);
     try {
       final user = await _googleSignIn.signIn();
+
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
-        print(user.photoUrl);
+        final googleAuth = await user.authentication;
+        final idToken = googleAuth.idToken;
+
+        final res = await _client.post(Uri.parse('$host/api/signup'),
+            body: jsonEncode({'idToken': idToken}),
+            headers: {'Content-Type': 'application/json; charset=UTF-8'});
+
+        switch (res.statusCode) {
+          case 200:
+            final Map<String, dynamic> userAccMap =
+                jsonDecode(res.body)['user'];
+
+            final newUser = UserModel.fromMap(userAccMap);
+
+            error = ErrorModel(error: null, data: newUser);
+            break;
+        }
       }
     } catch (e) {
-      print(e);
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
     }
+
+    return error;
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice_flutter_googledocs_clone/repository/auth_repository.dart';
-import 'package:practice_flutter_googledocs_clone/screens/colors.dart';
+import 'package:practice_flutter_googledocs_clone/colors.dart';
+import 'package:practice_flutter_googledocs_clone/screens/home_screen.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.watch(authRepositoryProvider).signInWithGoogle();
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final sMessenger = ScaffoldMessenger.of(context);
+    final navigator = Navigator.of(context);
+    //위로 빼는 이유: context 호출에 await로 gap을 주면, gap 이후에 widget이 존재한다고 보장하지 못하기 때문에, context가 destroy되었을 수 있다.
+    //context.mounted 로 체크하거나, await 전에 생성해 놓는다.
+
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+
+    if (errorModel.error == null) {
+      ref.read(userProvider.notifier).update((state) => errorModel.data);
+      //성공 응답에서 받은 데이터로 provider를 업데이트한다.
+
+      navigator
+          .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+    } else {
+      sMessenger.showSnackBar(
+        SnackBar(
+          content: Text(errorModel.error!),
+        ),
+      );
+    }
   }
 
   @override
@@ -17,7 +38,7 @@ class LoginScreen extends ConsumerWidget {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () => signInWithGoogle(ref),
+          onPressed: () => signInWithGoogle(ref, context),
           icon: Image.asset(
             'assets/images/g-logo-2.png',
             height: 20,
